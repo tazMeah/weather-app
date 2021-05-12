54a248f87f4d0b44486d7c4f236da073a4754c3d
tazMeah <montazmeahii@yahoo.com>
2021-05-12 00:41:57 -0400

    useEffect is returning weather for Detroit

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import getWeather from "../weatherApi";
+import React, { useState, useEffect } from "react";
+// import getWeather from "../weatherApi";
 import "./Result.css";
 
 function Result() {
@@ -7,10 +7,32 @@ function Result() {
     // to alter the state, initial state is empty
     const [weather, setWeather] = useState([]);
 
-    // for the initial load of data once api is done
+    
     useEffect(() => {
-        getWeather();
-    }, [])
+        // API call goes here.
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=c75be72de3dc82042431f27ccbf29521&units=imperial")
+            .then(res => res.json())
+            // .then(result => result)
+            .then(result => setWeather(result))
+            .then(function(){
+                console.log(weather)
+            })
+    }, []);
+
+
+
+
+    // cityName = result.name;
+    // description = result.weather[0].description;
+    // feelsLike = result.main.feels_like;
+    // wind = result.wind.speed;
+    // console.log(cityName, description, feelsLike, wind);
+
+
+
+
+
+
 
 
     return (
--- src/weatherApi.tsx
+++ src/weatherApi.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
 const api = {
@@ -9,12 +9,24 @@ const api = {
 
 
 
-function getWeather() {
+
+function GetWeather() {
+
+    const [weather, setWeather] = useState([]);
+
+    useEffect(() => {
+        GetWeather();
+        console.log(weather);
+    }, [])
+
     // "detroit" is hardcoded at the moment
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=c75be72de3dc82042431f27ccbf29521&units=imperial")
+     fetch("https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=c75be72de3dc82042431f27ccbf29521&units=imperial")
         .then(res => res.json())
-        .then(result => result)
+        // .then(result => result)
+        .then(result => setWeather(result))
+
+        // how to pass up "result" to the useState in the Result app that will call this function?
 }
 
-export default getWeather;
+export default GetWeather;
 
